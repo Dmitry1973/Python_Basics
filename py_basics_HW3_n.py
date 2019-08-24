9efb6a53172ac45a62783e0980c2b25378b4d6e2
Dmitry <39635520+Dmitry1973@users.noreply.github.com>
2019-08-25 00:50:11 +0200

    Update py_basics_HW3_n.py

--- py_basics_HW3_n.py
+++ py_basics_HW3_n.py
@@ -30,7 +30,12 @@ fibonacci(12, 10)
 
 
 def sort_to_max(origin_list):
-    pass
+    for i in range(len(origin_list), 0, -1):
+        for j in range(1, i):
+            if origin_list[j - 1] > origin_list[j]:
+                origin_list[j - 1], origin_list[j] = origin_list[j], origin_list[j - 1]
+    return print(origin_list)
+
 
 sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 
